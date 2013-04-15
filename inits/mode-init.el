1840b8645332f0a1e1f117c434b0c812983f6c39
amkkun <amkkun@gmail.com>
2013-04-15 14:48:58 +0900

    Move elisp files of ghc-mod

--- inits/mode-init.el
+++ inits/mode-init.el
@@ -1,6 +1,8 @@
 (provide 'mode-init)
 
 ;; ghc-mod
+(require 'config)
+(add-to-list 'load-path (concat elisp-dir "/ghc-mod"))
 (add-to-list 'exec-path
              (concat (getenv "HOME") "/.cabal-dev/bin"))
 (autoload 'ghc-init "ghc" nil t)
